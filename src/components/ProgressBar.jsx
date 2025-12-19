94d47666724ac8e59f2f131e934e938bb5830c84
Premanshu Patel <pmpatel@udel.edu>
2025-12-19 17:27:32 -0500

    Project Upload

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
+
+export default function ProgressBar({timer}){
+
+    const [remainingTime, setRemainingTime] = useState(timer);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log("interval");
+            setRemainingTime(prev => prev - 10);
+        }, 10);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+
+    return <progress value={remainingTime} max={timer}/>
+}
